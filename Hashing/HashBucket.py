a48baabab39367a22c220f281089f8ca105a9fa9
kuadc <contactocatalan@gmail.com>
2025-11-05 14:35:16 -0300

    update delete

--- Hashing/HashBucket.py
+++ Hashing/HashBucket.py
@@ -3,17 +3,17 @@ from Bucket import Bucket
 
 class HashBucket:
     __m:int
-    __c:int
-    __n:int
     __t:int
+    __c:int
     __array: np.array
+    
 
     def __init__(self, n=0, c=0):
-        self.__n = n
-        self.__m = n//c
         self.__c = c
+        self.__m = n//c
         self.__t = int((n//c) * 1.2)
         self.__array = np.zeros(self.__t, dtype=object)
+        self.__contador = np.zeros(self.__m,dtype=object)
 
     def myhashDivision(self, num):
         hash = num % self.__m
@@ -21,17 +21,19 @@ class HashBucket:
 
     def insertar(self, num):
         pos = self.myhashDivision
-        if self.__array[pos] == 0:
+        if self.__contador[pos] == 0:
             self.__array[pos] = Bucket(self.__c)
             self.__array[pos].insertar(num)
+            self.__contador[pos]+=1
             return
 
-        if self.__array[pos].buscar(num):
-            print("El Elemento ya existe")
-            return
-
-        if self.__array[pos].insertar(num):
-            print("Elemento insertado")
+        if self.__contador[pos] < self.__c:
+            if self.__array[pos].buscar():
+                print("Elemento ya ingresado")
+                return
+            else:
+                self.__array[pos].insertar(num)
+                self.__contador[pos]+=1
         else:
             self.overflow(num)
 
@@ -45,8 +47,13 @@ class HashBucket:
             print("El Elemento ya existe")
             return
 
+    def mostrar(self):
+        print(self.__array)
 
 
+if __name__ == "__main__":
+    b = HashBucket(30, 3)
+    b.mostrar()
 
 
 
